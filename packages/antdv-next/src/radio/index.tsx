093e9f843c8ab237210ef30f7b7526afce7ad4cc
shiqkuangsan <761105568@qq.com>
2026-02-15 22:21:42 +0800

    feat(radio): add withModel wrapper for Radio.Group v-model shorthand

--- packages/antdv-next/src/radio/index.tsx
+++ packages/antdv-next/src/radio/index.tsx
@@ -1,5 +1,6 @@
 import type { App } from 'vue'
 import type { CheckboxOptionType } from '../checkbox'
+import { withModel } from '../_util/withModel'
 import Group from './group'
 import Radio from './radio'
 import Button from './radioButton'
@@ -17,16 +18,16 @@ export type {
 } from './interface'
 
 export type RadioOptionType = CheckboxOptionType
-export const RadioGroup = Group
+export const RadioGroup = withModel(Group, { prop: 'value' }) as typeof Group
 export const RadioButton = Button
 
 ;(Radio as any).Button = Button
-;(Radio as any).Group = Group
+;(Radio as any).Group = RadioGroup
 ;(Radio as any).__ANT_RADIO = true
 
 ;(Radio as any).install = (app: App) => {
   app.component(Radio.name, Radio)
-  app.component(RadioGroup.name, RadioGroup)
+  app.component(Group.name, RadioGroup)
   app.component(RadioButton.name, RadioButton)
 }
 
